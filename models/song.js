4932c529976ebb216d23a97db33a86d6f3ad9626
Gabrielle Asha <167376964+gabasha00@users.noreply.github.com>
2025-12-18 05:39:17 -0500

    List HW Part 2
    Added functionality to let users add, edit, and delete entries

--- models/song.js
+++ models/song.js
@@ -1,8 +1,11 @@
 const mongoose = require("mongoose");
 
-const songSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  artist: { type: String, required: true },
-});
+const songSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true, trim: true },
+    artist: { type: String, required: true, trim: true },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Song", songSchema, "songs");
--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,41 @@ const Song = require("../models/song");
 
 router.get("/", async (req, res, next) => {
   try {
-    const songs = await Song.find();
+    const songs = await Song.find().sort({ createdAt: -1});
     res.render("index", { songs });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post("/item", async (req, res, next) => {
+  try {
+    const { title, artist } = req.body;
+    await Song.create({ title, artist });
+    res.redirect("/");
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.put("/item/:id", async (req, res, next) => {
+  try {
+    const { title, artist } = req.body;
+    await Song.findByIdAndUpdate(
+      req.params.id,
+      { title, artist },
+      { runValidators: true }
+    );
+    res.redirect("/");
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.delete("/item/:id", async (req, res, next) => {
+  try {
+    await Song.findByIdAndDelete(req.params.id);
+    res.redirect("/");
   } catch (err) {
     next(err);
   }
